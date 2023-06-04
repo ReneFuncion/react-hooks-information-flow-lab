36551511978add1f17059917de7f9fc53bef9699
Rene Funcion <rcfun959@gmail.com>
2023-06-03 23:14:19 -0700

    Completed react-hooks-information-flow-lab

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header({ onDarkModeClick, isDarkMode }) {
+  function handleClick() {
+    onDarkModeClick();
+  }
+  return (
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={handleClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </header>
+  );
+}
+
+export default Header;
